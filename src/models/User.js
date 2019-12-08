1d1595ab0854ed4751b3dab4b7f4635118e2a6b1
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2019-12-08 20:00:48 -0200

    Adição da classe User; Intanciando a classe

---
+++ src/models/User.js
@@ -0,0 +1,13 @@
+const { Model, DataTypes } = require('Sequelize');
+
+class Users extends Model{
+    static init(sequelize){
+        super.init({
+            name: DataTypes.STRING,
+            email: DataTypes.STRING,
+
+        }, { sequelize })
+    }
+}
+
+module.exports = Users;
