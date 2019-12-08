5d3a34ea84b4a2406009738efd6bb9ca9dfe3388
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2019-12-08 20:28:01 -0200

    Adição da migrate dos endereços

---
+++ src/database/migrations/20191208222102-create-addresses.js
@@ -0,0 +1,53 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('addresses', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      user_id:{
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: ''
+      },
+      zipcode: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      number: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
+/*
+* References --> ele define, aquele campo que o conter, como chave estrangeira
+* 
+* onUpdate  --> ele define p que fazer em caso a chave primaria for mudada
+*
+* onDelete --> ele define p que fazer em caso a chave primaria for deletada
+*
+*/
