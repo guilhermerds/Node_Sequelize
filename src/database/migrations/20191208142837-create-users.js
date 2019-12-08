231870c886893f43c4a4fb4aac177535d418f222
Guilherme <guilherme.rodriguesdasilva1010@gmail.com>
2019-12-08 13:02:40 -0200

    Adição da pasta migrations

---
+++ src/database/migrations/20191208142837-create-users.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', { 
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
+
+/*
+* type --> é o tipo do banco de dados
+* primaryKey --> define o campo como chave primária
+* autoIncrement --> o campo será auto preenchido
+* allowNull --> o campo não poderá ser nulo
+* 
+*/
